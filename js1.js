9137678b1cf2886610fb0f315e807eddea386afe
zFlashen <79690021+zFlashen@users.noreply.github.com>
2025-07-12 14:24:39 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -1830,10 +1830,10 @@ var version, showSideMenu, hideSideMenu;
                     _0x112A6 += _0x11233.score
                 } else {
                     if (_0x11233.state === _0x120EF.DEAD) {
-                        _0x11233.scoreText = "RIPP💀"
+                        _0x11233.scoreText = "💀"
                     } else {
                         if (_0x11233.state === _0x120EF.SPECTATE) {
-                            _0x11233.scoreText = "Spec👁️"
+                            _0x11233.scoreText = "👁️"
                         }
                     }
                 };
