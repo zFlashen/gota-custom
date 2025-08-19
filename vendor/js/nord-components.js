248349d87aeb1823af707dee643787068c0ea478
zFlashen <79690021+zFlashen@users.noreply.github.com>
2025-08-19 11:46:06 +0300

    Add files via upload

---
+++ vendor/js/nord-components.js
@@ -0,0 +1,285 @@
+export {
+    isTranslationRegistered,
+    registerTranslation
+}
+from "https://nordcdn.net/ds/components/4.2.0/translation.js";
+export {
+    default as Badge
+}
+from "https://nordcdn.net/ds/components/4.2.0/Badge.js";
+export {
+    default as Button
+}
+from "https://nordcdn.net/ds/components/4.2.0/Button.js";
+export {
+    default as Card
+}
+from "https://nordcdn.net/ds/components/4.2.0/Card.js";
+export {
+    default as Input
+}
+from "https://nordcdn.net/ds/components/4.2.0/Input.js";
+export {
+    default as Icon
+}
+from "https://nordcdn.net/ds/components/4.2.0/Icon.js";
+export {
+    default as CommandMenu
+}
+from "https://nordcdn.net/ds/components/4.2.0/CommandMenu.js";
+export {
+    default as CommandMenuAction
+}
+from "https://nordcdn.net/ds/components/4.2.0/CommandMenuAction.js";
+export {
+    default as Select
+}
+from "https://nordcdn.net/ds/components/4.2.0/Select.js";
+export {
+    default as Stack
+}
+from "https://nordcdn.net/ds/components/4.2.0/Stack.js";
+export {
+    default as Spinner
+}
+from "https://nordcdn.net/ds/components/4.2.0/Spinner.js";
+export {
+    default as Table
+}
+from "https://nordcdn.net/ds/components/4.2.0/Table.js";
+export {
+    default as VisuallyHidden
+}
+from "https://nordcdn.net/ds/components/4.2.0/VisuallyHidden.js";
+export {
+    default as Textarea
+}
+from "https://nordcdn.net/ds/components/4.2.0/Textarea.js";
+export {
+    C as Calendar
+}
+from "https://nordcdn.net/ds/components/4.2.0/Calendar-CZsJ8NUp.js";
+export {
+    default as DatePicker
+}
+from "https://nordcdn.net/ds/components/4.2.0/DatePicker.js";
+export {
+    default as Checkbox
+}
+from "https://nordcdn.net/ds/components/4.2.0/Checkbox.js";
+export {
+    default as Tooltip
+}
+from "https://nordcdn.net/ds/components/4.2.0/Tooltip.js";
+export {
+    default as Fieldset
+}
+from "https://nordcdn.net/ds/components/4.2.0/Fieldset.js";
+export {
+    default as Radio
+}
+from "https://nordcdn.net/ds/components/4.2.0/Radio.js";
+export {
+    default as Header
+}
+from "https://nordcdn.net/ds/components/4.2.0/Header.js";
+export {
+    default as Message
+}
+from "https://nordcdn.net/ds/components/4.2.0/Message.js";
+export {
+    default as NavGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/NavGroup.js";
+export {
+    default as NavItem
+}
+from "https://nordcdn.net/ds/components/4.2.0/NavItem.js";
+export {
+    default as Navigation
+}
+from "https://nordcdn.net/ds/components/4.2.0/Navigation.js";
+export {
+    default as Layout
+}
+from "https://nordcdn.net/ds/components/4.2.0/Layout.js";
+export {
+    default as EmptyState
+}
+from "https://nordcdn.net/ds/components/4.2.0/EmptyState.js";
+export {
+    default as Banner
+}
+from "https://nordcdn.net/ds/components/4.2.0/Banner.js";
+export {
+    default as Avatar
+}
+from "https://nordcdn.net/ds/components/4.2.0/Avatar.js";
+export {
+    default as ProgressBar
+}
+from "https://nordcdn.net/ds/components/4.2.0/ProgressBar.js";
+export {
+    P as Popout
+}
+from "https://nordcdn.net/ds/components/4.2.0/Popout-DigX3jOy.js";
+export {
+    default as Dropdown
+}
+from "https://nordcdn.net/ds/components/4.2.0/Dropdown.js";
+export {
+    default as DropdownItem
+}
+from "https://nordcdn.net/ds/components/4.2.0/DropdownItem.js";
+export {
+    default as DropdownGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/DropdownGroup.js";
+export {
+    default as TabGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/TabGroup.js";
+export {
+    default as Tab
+}
+from "https://nordcdn.net/ds/components/4.2.0/Tab.js";
+export {
+    default as TabPanel
+}
+from "https://nordcdn.net/ds/components/4.2.0/TabPanel.js";
+export {
+    default as Toggle
+}
+from "https://nordcdn.net/ds/components/4.2.0/Toggle.js";
+export {
+    default as Modal
+}
+from "https://nordcdn.net/ds/components/4.2.0/Modal.js";
+export {
+    default as Skeleton
+}
+from "https://nordcdn.net/ds/components/4.2.0/Skeleton.js";
+export {
+    default as Toast
+}
+from "https://nordcdn.net/ds/components/4.2.0/Toast.js";
+export {
+    default as ToastGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/ToastGroup.js";
+export {
+    default as Divider
+}
+from "https://nordcdn.net/ds/components/4.2.0/Divider.js";
+export {
+    default as Qrcode
+}
+from "https://nordcdn.net/ds/components/4.2.0/Qrcode.js";
+export {
+    default as Drawer
+}
+from "https://nordcdn.net/ds/components/4.2.0/Drawer.js";
+export {
+    N as NavToggle
+}
+from "https://nordcdn.net/ds/components/4.2.0/NavToggle-CXMNc13D.js";
+export {
+    default as Range
+}
+from "https://nordcdn.net/ds/components/4.2.0/Range.js";
+export {
+    default as ButtonGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/ButtonGroup.js";
+export {
+    default as Notification
+}
+from "https://nordcdn.net/ds/components/4.2.0/Notification.js";
+export {
+    default as NotificationGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/NotificationGroup.js";
+export {
+    default as TopBar
+}
+from "https://nordcdn.net/ds/components/4.2.0/TopBar.js";
+export {
+    default as Footer
+}
+from "https://nordcdn.net/ds/components/4.2.0/Footer.js";
+export {
+    default as Tag
+}
+from "https://nordcdn.net/ds/components/4.2.0/Tag.js";
+export {
+    default as TagGroup
+}
+from "https://nordcdn.net/ds/components/4.2.0/TagGroup.js";
+export {
+    default as SegmentedControl
+}
+from "https://nordcdn.net/ds/components/4.2.0/SegmentedControl.js";
+export {
+    default as SegmentedControlItem
+}
+from "https://nordcdn.net/ds/components/4.2.0/SegmentedControlItem.js";
+import "https://nordcdn.net/ds/components/4.2.0/en-us.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization2.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization3.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization4.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization5.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization6.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization7.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization8.js";
+import "https://nordcdn.net/ds/components/4.2.0/localization9.js";
+import "https://nordcdn.net/ds/components/4.2.0/custom-element-CrijKA9J.js";
+import "https://nordcdn.net/ds/components/4.2.0/lit-element-CDJJjKCD.js";
+import "https://nordcdn.net/ds/components/4.2.0/property-D6aRJ58V.js";
+import "https://nordcdn.net/ds/components/4.2.0/Component-CgWpfWT6.js";
+import "https://nordcdn.net/ds/components/4.2.0/ref-BreVms34.js";
+import "https://nordcdn.net/ds/components/4.2.0/directive-CrXa6qc1.js";
+import "https://nordcdn.net/ds/components/4.2.0/EventController-BBOmvfLa.js";
+import "https://nordcdn.net/ds/components/4.2.0/LightDomController-BIiLqdFB.js";
+import "https://nordcdn.net/ds/components/4.2.0/cond-BRE9RD0P.js";
+import "https://nordcdn.net/ds/components/4.2.0/FocusableMixin-4uYguezu.js";
+import "https://nordcdn.net/ds/components/4.2.0/InputMixin-CStYC0aJ.js";
+import "https://nordcdn.net/ds/components/4.2.0/SlotController-DfzJefii.js";
+import "https://nordcdn.net/ds/components/4.2.0/query-hiZs21fO.js";
+import "https://nordcdn.net/ds/components/4.2.0/if-defined-Du4H0BGt.js";
+import "https://nordcdn.net/ds/components/4.2.0/class-map-DclMK077.js";
+import "https://nordcdn.net/ds/components/4.2.0/style-map-DZ0b5R0N.js";
+import "https://nordcdn.net/ds/components/4.2.0/FormAssociatedMixin-D4Sc8-x0.js";
+import "https://nordcdn.net/ds/components/4.2.0/FormDataController-OUt5L5uC.js";
+import "https://nordcdn.net/ds/components/4.2.0/events-Bv6wNHwJ.js";
+import "https://nordcdn.net/ds/components/4.2.0/TextField-DDwuq3yM.js";
+import "https://nordcdn.net/ds/components/4.2.0/AutocompleteMixin-C_tXVTkf.js";
+import "https://nordcdn.net/ds/components/4.2.0/TextSelectableMixin-f7hEcczt.js";
+import "https://nordcdn.net/ds/components/4.2.0/SizeMixin-M_o6N_Tr.js";
+import "https://nordcdn.net/ds/components/4.2.0/FormField-BIYDbdAZ.js";
+import "https://nordcdn.net/ds/components/4.2.0/DirectionController-CBuCr1-e.js";
+import "https://nordcdn.net/ds/components/4.2.0/state-CLy7bemX.js";
+import "https://nordcdn.net/ds/components/4.2.0/IconManager.js";
+import "https://nordcdn.net/ds/components/4.2.0/observe-D0n0zOfU.js";
+import "https://nordcdn.net/ds/components/4.2.0/collection-L_vDvn-S.js";
+import "https://nordcdn.net/ds/components/4.2.0/number-Dg2vCfGd.js";
+import "https://nordcdn.net/ds/components/4.2.0/LightDismissController-4pH8cdko.js";
+import "https://nordcdn.net/ds/components/4.2.0/ShortcutController-BIb3WGzH.js";
+import "https://nordcdn.net/ds/components/4.2.0/tinykeys.module-_6MZt7MP.js";
+import "https://nordcdn.net/ds/components/4.2.0/KeyboardController.js";
+import "https://nordcdn.net/ds/components/4.2.0/SelectEvent.js";
+import "https://nordcdn.net/ds/components/4.2.0/LocalizeController.js";
+import "https://nordcdn.net/ds/components/4.2.0/dates-lbaNvUdr.js";
+import "https://nordcdn.net/ds/components/4.2.0/DateSelectEvent.js";
+import "https://nordcdn.net/ds/components/4.2.0/interface-close-small-CnpAFMO3.js";
+import "https://nordcdn.net/ds/components/4.2.0/date-adapter.js";
+import "https://nordcdn.net/ds/components/4.2.0/interface-checked-small-BtICPEsp.js";
+import "https://nordcdn.net/ds/components/4.2.0/positioning-CsfLhMSW.js";
+import "https://nordcdn.net/ds/components/4.2.0/fsm-Bq5jMQrK.js";
+import "https://nordcdn.net/ds/components/4.2.0/LightSlotController-C3e_WyzU.js";
+import "https://nordcdn.net/ds/components/4.2.0/DropdownItem-C7ktXPSd.js";
+import "https://nordcdn.net/ds/components/4.2.0/Sticky-TVZ4rUPp.js";
+import "https://nordcdn.net/ds/components/4.2.0/ScrollbarController-C5CMxfb3.js";
+import "https://nordcdn.net/ds/components/4.2.0/ModalController.js";
+import "https://nordcdn.net/ds/components/4.2.0/NotificationMixin-D7BQPdb7.js";
+//# sourceMappingURL=index.js.map
